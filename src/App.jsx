056b4f7829e443a650e1dde0f1c8f489630adb7c
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-07-29 20:13:49 +0100

    Create components & implement Clarifai api

---
+++ src/App.jsx
@@ -0,0 +1,92 @@
+import React, {Component} from "react";
+
+import Clarifai from 'clarifai';
+import Particles from 'react-particles-js';
+
+import Navigation from "./components/Navigation/Navigation";
+import Logo from "./components/Logo/Logo";
+import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
+import Rank from "./components/Rank/Rank";
+import FaceDetection from "./components/FaceDetection/FaceDetection";
+
+import "./App.css";
+
+//You must add your own API key here from Clarifai.
+const app = new Clarifai.App({
+ apiKey: 'YOUR_API_KEY'
+});
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {}
+    }
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    }
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({box: box});
+  }
+
+  onInputChange = event => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+    this.setState({imageUrl: this.state.input});
+    app.models
+      .predict(
+         Clarifai.FACE_DETECT_MODEL,
+         this.state.input)
+      .then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    const {box, imageUrl } = this.state;
+    return (
+      <div className="App">
+        <Particles 
+          className='particles'
+          params={particlesOptions} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+           />
+        <FaceDetection box={box} imageUrl={imageUrl} />
+      </div>
+    );
+  }
+}
+
+export default App;
