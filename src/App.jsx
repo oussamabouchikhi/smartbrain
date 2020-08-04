e0894ca7780384fd9fdc9837253ef968646ab35b
oussamabouchikhi <oussamabouchikhi700@gmail.com>
2020-08-04 15:11:36 +0100

    Clear everything after user signout

--- src/App.jsx
+++ src/App.jsx
@@ -29,23 +29,25 @@ const particlesOptions = {
   }
 }
 
+const INITIAL_STATE = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+
 class App extends Component {
   constructor() {
     super();
-    this.state = {
-      input: '',
-      imageUrl: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''
-      }
-    }
+    this.state = INITIAL_STATE;
   }
 
   loadUser = (data) => {
@@ -98,7 +100,7 @@ class App extends Component {
             .then(count => {
               this.setState(Object.assign(this.state.user, { entries: count}))
             })
-
+            .catch(console.log);
         }
         this.displayFaceBox(this.calculateFaceLocation(response))
       })
@@ -107,7 +109,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(INITIAL_STATE)
     } else if (route === 'home') {
       this.setState({isSignedIn: true})
     }
